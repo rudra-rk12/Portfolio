e5f60abef5d0643a288ba7f8e0cc206a5139b76f
Rudraksha Kokane <rudrark0109@gmail.com>
2024-09-20 00:36:50 -0500

    Routing Error Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+import React from 'react'
 import './App.css';
-import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
@@ -8,19 +8,15 @@ import Projects from './Pages/Projects';
 
 function App() {
   return (
-    <div>
-        <Navbar/>
-
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" component={Homepage}/>
-            <Route path="/qualifications"component={Qualifications}/>
-            <Route path="/projects" components={Projects}/>
-          </Routes>
-        </BrowserRouter>
-
-        <Footer/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/qualifications" element={<Qualifications />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,9 +7,7 @@ function Navbar() {
             <header>
                 <nav className="navbar navbar-expand-lg bg-body-secondary">
                     <div className="container-fluid">
-                        <a className="navbar-brand title-font" href="/">
-                            PortFolio
-                        </a>
+                            <Link className="navbar-brand title-font" to="/">Rudraksha</Link>
                         <button
                             className="navbar-toggler"
                             type="button"
@@ -21,32 +19,29 @@ function Navbar() {
                         >
                             <span className="navbar-toggler-icon" />
                         </button>
+                        
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">
-                                        <Link to='/'>Home</Link>
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/">
-                                        <Link to='/projects'>Projects</Link>
-                                    </a>
+                                    <Link className="nav-link" to='/'>Home</Link>
                                 </li>
 
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">
-                                        <Link to="/qualifications">Qualifications</Link>
-                                    </a>
+                                        <Link className="nav-link" to='/projects'>Projects</Link>
+                                </li>
+
+                                <li className="nav-item">
+                                        <Link className="nav-link" to="/qualifications">Qualifications</Link>
                                 </li>
                             </ul>
+
                             <div className="d-flex">
-                                <button className="btn btn-outline-primary profile-links github" type="submit" onClick={() => { window.open('https://github.com/rudra-rk12') } }>
+                                <button className="btn btn-outline-primary profile-links github" type="submit" onClick={() => { window.open('https://github.com/rudra-rk12', '_blank', 'noopener noreferrer') } }>
                                     Github
                                 </button>
                             </div>
                             <div className="d-flex">
-                                <button className="btn btn-outline-primary profile-links linkedin" type="submit" onClick={() => { window.open('https://www.linkedin.com/in/rudrark12') } }>
+                                <button className="btn btn-outline-primary profile-links linkedin" type="submit" onClick={() => { window.open('https://www.linkedin.com/in/rudrark12', '_blank', 'noopener noreferrer') } }>
                                     LinkedIn
                                 </button>
                             </div>
